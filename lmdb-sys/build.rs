be8e1267e69461d25e574db58a4ee2df1136f050
Simon Leier <simonleier@gmail.com>
2020-10-03 21:55:20 +0200

    Move lmdb into project

---
+++ lmdb-sys/build.rs
@@ -0,0 +1,36 @@
+extern crate cc;
+extern crate pkg_config;
+
+#[cfg(feature = "bindgen")]
+extern crate bindgen;
+
+#[cfg(feature = "bindgen")]
+#[path = "bindgen.rs"]
+mod generate;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    #[cfg(feature = "bindgen")]
+    generate::generate();
+
+    let mut lmdb = PathBuf::from(&env::var("CARGO_MANIFEST_DIR").unwrap());
+    lmdb.push("lmdb");
+    lmdb.push("libraries");
+    lmdb.push("liblmdb");
+
+    if !pkg_config::find_library("liblmdb").is_ok() {
+        let mut builder = cc::Build::new();
+
+        builder
+            .file(lmdb.join("mdb.c"))
+            .file(lmdb.join("midl.c"))
+            // https://github.com/mozilla/lmdb/blob/b7df2cac50fb41e8bd16aab4cc5fd167be9e032a/libraries/liblmdb/Makefile#L23
+            .flag_if_supported("-Wno-unused-parameter")
+            .flag_if_supported("-Wbad-function-cast")
+            .flag_if_supported("-Wuninitialized");
+
+        builder.compile("liblmdb.a")
+    }
+}
---
+++ lmdb-sys/src/lib.rs
@@ -0,0 +1,21 @@
+#![deny(warnings)]
+#![allow(non_camel_case_types)]
+#![allow(clippy::all)]
+
+extern crate libc;
+
+#[cfg(unix)]
+#[allow(non_camel_case_types)]
+pub type mdb_mode_t = ::libc::mode_t;
+#[cfg(windows)]
+#[allow(non_camel_case_types)]
+pub type mdb_mode_t = ::libc::c_int;
+
+#[cfg(unix)]
+#[allow(non_camel_case_types)]
+pub type mdb_filehandle_t = ::libc::c_int;
+#[cfg(windows)]
+#[allow(non_camel_case_types)]
+pub type mdb_filehandle_t = *mut ::libc::c_void;
+
+include!("bindings.rs");
