b6343ef94970d2d4858648aba9394ef1eab01db2
Simon Leier <simonleier@gmail.com>
2021-01-03 21:41:20 +0100

    Update crud.rs

--- dart-ffi/src/crud.rs
+++ dart-ffi/src/crud.rs
@@ -1,19 +1,18 @@
 use crate::async_txn::{AsyncResponse, IsarAsyncTxn};
-use crate::raw_object_set::{RawObject, RawObjectSend, RawObjectSet};
+use crate::raw_object_set::{RawObject, RawObjectSend};
 use isar_core::collection::IsarCollection;
 use isar_core::error::Result;
 use isar_core::txn::IsarTxn;
 
 #[no_mangle]
 pub unsafe extern "C" fn isar_get(
-    collection: Option<&IsarCollection>,
-    txn: Option<&IsarTxn>,
+    collection: &IsarCollection,
+    txn: &IsarTxn,
     object: &mut RawObject,
 ) -> u8 {
     isar_try! {
-        let collection = collection.unwrap();
         let object_id = object.get_object_id(collection).unwrap();
-        let result = collection.get(txn.unwrap(), object_id)?;
+        let result = collection.get(txn, object_id)?;
         if let Some(result) = result {
             object.set_object(result);
         } else {
@@ -24,13 +23,13 @@ pub unsafe extern "C" fn isar_get(
 
 #[no_mangle]
 pub unsafe extern "C" fn isar_get_async(
-    collection: Option<&'static IsarCollection>,
-    txn: Option<&IsarAsyncTxn>,
-    object: RawObjectSend,
+    collection: &'static IsarCollection,
+    txn: &IsarAsyncTxn,
+    object: &'static mut RawObject,
 ) {
-    let collection = collection.unwrap();
+    let object = RawObjectSend(object);
     let oid = object.0.get_object_id(collection).unwrap();
-    txn.unwrap().exec(move |txn| -> Result<AsyncResponse> {
+    txn.exec(move |txn| -> Result<AsyncResponse> {
         let result = collection.get(txn, oid)?;
         if let Some(result) = result {
             object.0.set_object(result);
@@ -43,28 +42,27 @@ pub unsafe extern "C" fn isar_get_async(
 
 #[no_mangle]
 pub unsafe extern "C" fn isar_put(
-    collection: Option<&mut IsarCollection>,
-    txn: Option<&mut IsarTxn>,
+    collection: &mut IsarCollection,
+    txn: &mut IsarTxn,
     object: &mut RawObject,
 ) -> u8 {
     isar_try! {
-        let collection = collection.unwrap();
         let oid = object.get_object_id(collection);
         let data = object.object_as_slice();
-        let oid = collection.put(txn.unwrap(), oid, data)?;
+        let oid = collection.put(txn, oid, data)?;
         object.set_object_id(oid);
     }
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn isar_put_async(
-    collection: Option<&'static IsarCollection>,
-    txn: Option<&IsarAsyncTxn>,
-    object: RawObjectSend,
+    collection: &'static IsarCollection,
+    txn: &IsarAsyncTxn,
+    object: &'static mut RawObject,
 ) {
-    let collection = collection.unwrap();
+    let object = RawObjectSend(object);
     let oid = object.0.get_object_id(collection);
-    txn.unwrap().exec(move |txn| -> Result<AsyncResponse> {
+    txn.exec(move |txn| -> Result<AsyncResponse> {
         let data = object.0.object_as_slice();
         let oid = collection.put(txn, oid, data)?;
         object.0.set_object_id(oid);
@@ -74,23 +72,18 @@ pub unsafe extern "C" fn isar_put_async(
 
 #[no_mangle]
 pub unsafe extern "C" fn isar_delete(
-    collection: Option<&IsarCollection>,
-    txn: Option<&mut IsarTxn>,
+    collection: &IsarCollection,
+    txn: &mut IsarTxn,
     object: &RawObject,
 ) -> u8 {
     isar_try! {
-        let collection = collection.unwrap();
-        collection.delete(txn.unwrap(), object.get_object_id(collection).unwrap())?;
+        collection.delete(txn, object.get_object_id(collection).unwrap())?;
     }
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn isar_clear(
-    collection: Option<&IsarCollection>,
-    txn: Option<&mut IsarTxn>,
-) -> u8 {
+pub unsafe extern "C" fn isar_clear(collection: &IsarCollection, txn: &mut IsarTxn) -> u8 {
     isar_try! {
-        let collection = collection.unwrap();
-        collection.clear(txn.unwrap())?;
+        collection.clear(txn)?;
     }
 }
