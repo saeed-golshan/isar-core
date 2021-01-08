d507397bac6304483a5cc1ce8bce244da6d82d59
Simon Leier <simonleier@gmail.com>
2021-01-08 09:25:08 +0100

    arm64 and x64 build

--- dart-ffi/tools/build_desktop.sh
+++ dart-ffi/tools/build_desktop.sh
@@ -4,12 +4,21 @@ cargo build --release
 
 case $(uname | tr '[:upper:]' '[:lower:]') in
   linux*)
-    mv "target/release/libisar_core_dart_ffi.so" "libisar_linux.so"
+    cargo build --release
+    mv "target/release/libisar_core_dart_ffi.so" "libisar_linux_x64.so"
     ;;
   darwin*)
-    mv "target/release/libisar_core_dart_ffi.dylib" "libisar_macos.dylib"
+    if [ "$1" = "x64" ]; then
+      rustup target add x86_64-apple-darwin
+      cargo build --release --target x86_64-apple-darwin
+      mv "target/x86_64-apple-darwin/release/libisar_core_dart_ffi.dylib" "libisar_macos_x64.dylib"
+    else
+      rustup target add aarch64-apple-darwin
+      cargo build --release --target aarch64-apple-darwin
+      mv "target/aarch64-apple-darwin/release/libisar_core_dart_ffi.dylib" "libisar_macos.dylib"
+    fi
     ;;
   *)
-    mv "target/release/isar_core_dart_ffi.dll" "isar_windows.dll"
+    mv "target/release/isar_core_dart_ffi.dll" "isar_windows_x64.dll"
     ;;
 esac
